210135295f6809e32763ac5096dd8ab15864b80b
Tomer Caspi <tomer_c@optimove.com>
2016-09-01 01:14:35 +0300

    Adding files to project

---
+++ src/homework.js
@@ -0,0 +1,49 @@
+/*
+ Using the example data set,
+ compute the average age difference between mothers and children (the age of the mother when the child is born).
+ Note that not all the mothers mentioned in the data are themselves present in the array.
+ You can create a helper object, which will make it easy to find a person’s object from their name.
+ */
+
+var myData = JSON.parse(ANCESTRY_FILE);
+
+/**
+ * return the position of the object if the name is equal
+ * */
+function isExists(targetArr, motherName){
+    for (var i=0; i< targetArr.length-1; i++){
+        if (targetArr[i].name === motherName)
+            return i;
+    }
+    return -1;
+}
+
+/**
+ * returns mothers average age from an array of objects
+ * */
+function getMotherAverageAge(data){
+    var sumOfAge = 0,
+        position,
+        counter = 0;
+
+    for (var i=0; i < data.length-1; i++){
+        if(data[i].mother != null) {
+            position = isExists(data, data[i].mother);
+            if (position != -1) {
+                sumOfAge += data[i].born - data[position].born;
+                counter++;
+            }
+        }
+    }
+
+    if (counter) {
+        console.log("There are " + counter + " mothers found in the array");
+        return (sumOfAge / counter).toFixed(2);
+    }
+    else
+        return 0;
+}
+
+document.write("The mothers Average Age is: " + getMotherAverageAge(myData));
+
+
